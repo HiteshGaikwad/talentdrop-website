ec32f4e198eb416941fc4af88bc43df12ee99c9d
Hitesh <gaikwadhitesh03@gmail.com>
2023-08-12 15:45:08 +0530

    resolve pages redirection

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react"
 import "../Footer.css"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import TermsConditions from "./TermsConditions"
 import Privacy from "./Privacy"
+import { Navigate } from "react-router-dom"
 
 
 const Footer= ()=>{
+
+    let navigate= useNavigate();
 
 
     return (
@@ -14,9 +17,12 @@ const Footer= ()=>{
             <div className="footer">
                 <div className="footer-left">
                     <p>© 2023  Talentdrop. All rights reserved.</p>
-                    <NavLink className={"footer-about"} to={"/about"} target={"_parent"}>About</NavLink>
+                    {/* <NavLink className={"footer-about"} to={"/about"} target={"_parent"}>About</NavLink>
                     <NavLink className={"footer-terms"} to={"/termsConditions"} target="_parent">Terms & Conditions</NavLink>
-                    <NavLink className={"footer-privacy"} to={"/privacy"}target="_parent">Privacy Policy</NavLink>
+                    <NavLink className={"footer-privacy"} to={"/privacy"}target="_parent">Privacy Policy</NavLink> */}
+                    <button  className={"footer-about"} onClick={()=>navigate("/about")} >About</button>
+                    <button className={"footer-terms"} onClick={()=>navigate("/termsConditions")} >Terms & Conditions</button>
+                    <button className={"footer-privacy"} onClick={()=>navigate("/privacy")} >Privacy Policy</button>
                 </div>
                 <div className="footer-right">
                 <NavLink to={"https://twitter.com/i/flow/login?redirect_after_login=%2FTalentdrop_Inc"} target={"_blank"}>
